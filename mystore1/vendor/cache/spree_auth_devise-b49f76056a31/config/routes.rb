f0b4b729c3351153996955645496cf8be098b1f0
Pete Argent <peter.argent@gmail.com>
2013-06-05 12:00:02 +1000

    bundle cache

---
+++ mystore1/vendor/cache/spree_auth_devise-b49f76056a31/config/routes.rb
@@ -0,0 +1,35 @@
+Spree::Core::Engine.routes.draw do
+  devise_for :spree_user,
+             :class_name => 'Spree::User',
+             :controllers => { :sessions => 'spree/user_sessions',
+                               :registrations => 'spree/user_registrations',
+                               :passwords => 'spree/user_passwords' },
+             :skip => [:unlocks, :omniauth_callbacks],
+             :path_names => { :sign_out => 'logout' },
+             :path_prefix => :user
+end
+
+Spree::Core::Engine.routes.prepend do
+  resources :users, :only => [:edit, :update]
+
+  devise_scope :spree_user do
+    get '/login' => 'user_sessions#new', :as => :login
+    get '/logout' => 'user_sessions#destroy', :as => :logout
+    get '/signup' => 'user_registrations#new', :as => :signup
+  end
+
+  match '/checkout/registration' => 'checkout#registration', :via => :get, :as => :checkout_registration
+  match '/checkout/registration' => 'checkout#update_registration', :via => :put, :as => :update_checkout_registration
+
+  resource :session do
+    member do
+      get :nav_bar
+    end
+  end
+
+  resource :account, :controller => 'users'
+
+  namespace :admin do
+    resources :users
+  end
+end
